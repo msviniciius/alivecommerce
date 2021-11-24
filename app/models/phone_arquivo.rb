47d0178ebd3fe4ffe19c8b092defdc0d77fe77ea
msvinicius <msvinicius.ads@gmail.com>
2021-11-24 12:49:50 -0800

    add has_many phone/phone_arquivo

--- app/models/phone_arquivo.rb
+++ app/models/phone_arquivo.rb
@@ -1,2 +1,6 @@
 class PhoneArquivo < ApplicationRecord
+  has_many :phones, dependent: :destroy
+
+  accepts_nested_attributes_for :phones, allow_destroy: true
+  validates :mes, :ano, presence: true
 end
