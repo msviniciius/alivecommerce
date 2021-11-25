28b2b3f9daa39093f13b2c7bca2b9c4c33ed0eb1
msvinicius <msvinicius.ads@gmail.com>
2021-11-25 12:51:59 -0800

    fix remove migration error

--- db/migrate/20211124211814_remove_phone_arquivo_from_phones.rb
+++
@@ -1,5 +0,0 @@
-class RemovePhoneArquivoFromPhones < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :phones, :phone_arquivos, foreign_key: true
-  end
-end
--- db/migrate/20211124211948_add_phone_arquivo_to_phones.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneArquivoToPhones < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :phones, :phone_arquivo, foreign_key: true
-  end
-end
