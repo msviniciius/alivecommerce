29d10be92085ce36b8bf054f60aa82e2ad176ec3
msvinicius <msvinicius.ads@gmail.com>
2021-11-25 11:34:16 -0800

    update tes phone_arquivos

--- spec/factories/phone_arquivos.rb
+++ spec/factories/phone_arquivos.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :phone_arquivo do
-    mes { "MyString" }
-    ano { "MyString" }
-    file { "MyString" }
+    ano { '2021' }
+    mes { '04' }
+    file { nil }
+
+    trait :file do
+      file { build_from_csv("#{Rails.root}/spec/support/assets/phones.csv", 'text/csv') }
+    end
+
+    trait :com_file_envio_invalido do
+      arquivo_envio { build_from_csv("#{Rails.root}/spec/support/assets/teste.xlsx", 'application/excel') }
+    end
   end
 end
--- spec/models/phone_arquivo_spec.rb
+++ spec/models/phone_arquivo_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PhoneArquivo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associações' do
+    it { is_expected.to have_many(:phones).class_name('Phone') }
+  end
+
+  describe 'validações' do
+    it 'é válido com atributos válidos' do
+      phone_arquivo = build(:phone_arquivo)
+      expect(phone_arquivo).to be_valid
+    end
+    it 'é inválido sem ano' do
+      is_expected.to validate_presence_of(:ano)
+    end
+    it 'é inválido sem mes' do
+      is_expected.to validate_presence_of(:mes)
+    end
+    
+    context 'ao importar do arquivo de envio' do
+      it 'é inválido se tipo :file diferente de csv' do
+        phone_arquivo = build(:phone_arquivo)
+        phone_arquivo.valid?
+        expect(phone_arquivo.errors[:build_from_csv]).to_not eq('deve ser no formato csv ou txt')
+      end
+    end
+  end
 end
