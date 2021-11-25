122390b2003ef00292e627b79c5dd6d68e5a1727
msvinicius <msvinicius.ads@gmail.com>
2021-11-24 18:32:08 -0800

    correcoes views/phones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
       "error"
     end
   end
+
+  def select_menu(controller, action=nil)
+    if controller_name == controller and (!action or action_name == action)
+      if action_name == "ballances"
+        if action and action == "ballances"
+          "class=active"
+        end
+      else
+        "class=active"
+      end
+    end
+  end
 end
--- app/views/phones/_phone.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! phone, :id, :model, :brand, :manufacturer, :chip_type, :so_version, :screen_type, :display_size, :resolution, :back_cam, :front_cam, :ram, :processor, :memory_int, :color, :modaly, :quantity, :price, :phone_arquivo_id, :created_at, :updated_at
-json.url phone_url(phone, format: :json)
