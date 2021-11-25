731cc70b4e7e53fe6901518b78344eaa088f3e9b
msvinicius <msvinicius.ads@gmail.com>
2021-11-25 11:34:58 -0800

    update test phones

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Phone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associações' do
+    it { is_expected.to belong_to(:phone_arquivo).class_name('PhoneArquivo') }
+  end
 end
